5f19b1cb843b62e9af50ac3480d236e384c4a4e3
dbottillo <dbottillo@gmail.com>
2014-03-25 20:45:52 +0000

    bug fixes

--- mtgSearch/src/main/java/com/dbottillo/base/DBFragment.java
+++ mtgSearch/src/main/java/com/dbottillo/base/DBFragment.java
@@ -52,7 +52,7 @@ public abstract class DBFragment extends DialogFragment {
         AdRequest adRequest = new AdRequest.Builder()
                 .setGender(AdRequest.GENDER_MALE)
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .addTestDevice("512F56FD659AA867F7F67C7066A85F33")
+                //.addTestDevice("512F56FD659AA867F7F67C7066A85F33")
                 .addTestDevice("81CB49F53C9C0C74241CB8BD3383E1C7")
                 .build();
         return adRequest;
