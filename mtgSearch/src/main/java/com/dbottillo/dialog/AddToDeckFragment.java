a13a6bcfb1aee016a52e52bd73657db0c4fb0ad3
dbottillo <dbottillo@gmail.com>
2015-09-17 19:36:38 +0100

    better popup add card to deck

--- mtgSearch/src/main/java/com/dbottillo/dialog/AddToDeckFragment.java
+++ mtgSearch/src/main/java/com/dbottillo/dialog/AddToDeckFragment.java
@@ -4,13 +4,18 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.dbottillo.R;
@@ -30,6 +35,11 @@ public class AddToDeckFragment extends DBFragment implements View.OnClickListene
     String[] decksChoose;
     String[] quantityChoose;
 
+    TextInputLayout cardNameInputLayout;
+    EditText deckName;
+    TextInputLayout cardQuantityInputLayout;
+    EditText cardQuantity;
+
     ArrayList<Deck> decks;
     MTGCard card;
 
@@ -42,6 +52,12 @@ public class AddToDeckFragment extends DBFragment implements View.OnClickListene
         chooseDeck = (Spinner) v.findViewById(R.id.choose_deck);
         chooseQuantity = (Spinner) v.findViewById(R.id.choose_quantity);
         sideboard = (CheckBox) v.findViewById(R.id.add_to_deck_sideboard);
+
+        cardNameInputLayout = (TextInputLayout) v.findViewById(R.id.new_deck_name_input_layout);
+        deckName = (EditText) v.findViewById(R.id.new_deck_name);
+        cardQuantityInputLayout = (TextInputLayout) v.findViewById(R.id.new_deck_quantity_input_layout);
+        cardQuantity = (EditText) v.findViewById(R.id.new_deck_quantity);
+        cardQuantity.setFilters(new InputFilter[]{ new InputFilterMinMax(1,30)});
 
         v.findViewById(R.id.add_to_deck_save).setOnClickListener(this);
 
@@ -63,10 +79,25 @@ public class AddToDeckFragment extends DBFragment implements View.OnClickListene
     }
 
     private void setupQuantitySpinner() {
-        quantityChoose = new String[]{"Choose quantity", "1", "2", "3", "4"};
+        quantityChoose = new String[]{getString(R.string.deck_choose_quantity), "1", "2", "3", "4", getString(R.string.deck_specify)};
         final ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(getActivity(), R.layout.add_to_deck_spinner_item, quantityChoose);
         adapter.setDropDownViewResource(R.layout.add_to_deck_dropdown_item);
         chooseQuantity.setAdapter(adapter);
+        chooseQuantity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 5) {
+                    chooseQuantity.setVisibility(View.GONE);
+                    cardQuantityInputLayout.setVisibility(View.VISIBLE);
+                    cardQuantity.requestFocus();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
@@ -90,32 +121,73 @@ public class AddToDeckFragment extends DBFragment implements View.OnClickListene
         return instance;
     }
 
-    private void setupDecksSpinner(ArrayList<Deck> decks) {
+    private void setupDecksSpinner(final ArrayList<Deck> decks) {
         this.decks = decks;
-        decksChoose = new String[decks.size() + 1];
-        decksChoose[0] = "Choose deck";
+        decksChoose = new String[decks.size() + 2];
+        decksChoose[0] = getString(R.string.deck_choose);
         int i = 1;
         for (Deck deck : decks) {
             decksChoose[i] = deck.getName();
             i++;
         }
+        decksChoose[decks.size()+1] = getString(R.string.deck_new);
         final ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(getActivity(), R.layout.add_to_deck_spinner_item, decksChoose);
         adapter.setDropDownViewResource(R.layout.add_to_deck_dropdown_item);
         chooseDeck.setAdapter(adapter);
+        chooseDeck.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == decks.size() + 1) {
+                    chooseDeck.setVisibility(View.GONE);
+                    cardNameInputLayout.setVisibility(View.VISIBLE);
+                    deckName.requestFocus();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        if (chooseQuantity.getSelectedItemPosition() > 0 && chooseDeck.getSelectedItemPosition() > 0) {
-            int quantity = Integer.parseInt(quantityChoose[chooseQuantity.getSelectedItemPosition()]);
-            Deck deck = decks.get(chooseDeck.getSelectedItemPosition() - 1);
-            boolean side = sideboard.isChecked();
-            saveCard(quantity, deck, side);
-            dismiss();
+        int quantity = -1;
+        if (chooseQuantity.getVisibility() == View.VISIBLE && chooseQuantity.getSelectedItemPosition() > 0){
+            quantity = Integer.parseInt(quantityChoose[chooseQuantity.getSelectedItemPosition()]);
+        }
+        if (chooseQuantity.getVisibility() == View.GONE && cardQuantity.getText().length() > 0){
+            quantity = Integer.parseInt(cardQuantity.getText().toString());
+        }
+        if (quantity > -1) {
+            if (chooseDeck.getVisibility() == View.VISIBLE && chooseDeck.getSelectedItemPosition() > 0) {
+                Deck deck = decks.get(chooseDeck.getSelectedItemPosition() - 1);
+                boolean side = sideboard.isChecked();
+                saveCard(quantity, deck, side);
+                dismiss();
+            }
+            if (chooseDeck.getVisibility() == View.GONE && deckName.getText().length() > 0) {
+                boolean side = sideboard.isChecked();
+                saveCard(quantity, deckName.getText().toString(), side);
+                dismiss();
+            }
         }
     }
 
     private void saveCard(final int quantity, final Deck deck, final boolean side) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                DeckDataSource deckDataSource = new DeckDataSource(getActivity().getApplicationContext());
+                deckDataSource.open();
+                deckDataSource.addCardToDeck(deck.getId(), card, quantity, side);
+            }
+        };
+        thread.start();
+    }
+
+    private void saveCard(final int quantity, final String deck, final boolean side) {
         Thread thread = new Thread() {
             @Override
             public void run() {
@@ -146,6 +218,30 @@ public class AddToDeckFragment extends DBFragment implements View.OnClickListene
         protected void onPostExecute(ArrayList<Deck> decks) {
             super.onPostExecute(decks);
             setupDecksSpinner(decks);
+        }
+    }
+
+    public class InputFilterMinMax implements InputFilter {
+
+        private int min, max;
+
+        public InputFilterMinMax(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            try {
+                int input = Integer.parseInt(dest.toString() + source.toString());
+                if (isInRange(min, max, input))
+                    return null;
+            } catch (NumberFormatException nfe) { }
+            return "";
+        }
+
+        private boolean isInRange(int a, int b, int c) {
+            return b > a ? c >= a && c <= b : c >= b && c <= a;
         }
     }
 
