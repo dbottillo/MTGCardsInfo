f0f7d187ecaf50c84d1e2cdc3a5e254895cbc7dc
dbottillo <dbottillo@gmail.com>
2015-11-29 00:53:51 +0000

    test depedencies

---
+++ mtgSearch/src/androidTest/java/com/dbottillo/base/MTGAppTest.java
@@ -0,0 +1,15 @@
+package com.dbottillo.base;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MTGAppTest {
+
+    @Test
+    public void first() {
+        assertThat(1, is(1));
+    }
+
+}
