a3fa5eb03a6e61178137e7b9043953fe90eccb98
dbottillo <dbottillo@gmail.com>
2015-12-24 16:04:42 +0000

    database test onUpgrade

--- mtgSearch/src/androidTest/java/com/dbottillo/mtgsearchfree/database/BaseDatabaseTest.java
+++ mtgSearch/src/androidTest/java/com/dbottillo/mtgsearchfree/database/BaseDatabaseTest.java
@@ -3,7 +3,6 @@ package com.dbottillo.mtgsearchfree.database;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 import android.test.RenamingDelegatingContext;
-import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,14 +10,12 @@ import org.junit.Before;
 public class BaseDatabaseTest {
 
     CardsInfoDbHelper dataHelper;
-    private RenamingDelegatingContext context;
+    protected RenamingDelegatingContext context;
 
     @Before
     public void create_data_helper() throws Exception {
-        context = new RenamingDelegatingContext(InstrumentationRegistry.getInstrumentation().getContext(), "test_");
+        context = new RenamingDelegatingContext(InstrumentationRegistry.getInstrumentation().getTargetContext(), "test_");
         dataHelper = new CardsInfoDbHelper(context);
-        Log.e("test", dataHelper.toString());
-        Log.e("test", dataHelper.getReadableDatabase().toString());
         dataHelper.clear();
     }
 
