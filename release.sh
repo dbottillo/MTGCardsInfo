a42f9ed5a4fd327c712d3b561fd2529a7b17ff85
dbottillo <dbottillo@gmail.com>
2016-01-21 17:51:16 +0000

    release note

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 gradle check
+gradle connectedCheck
 gradle assembleRelease --no-daemon
 cp -r MTGSearch/build/outputs/apk/MTGSearch-release.apk ~/Google\ Drive/MTGCardsInfo/MTGSearch.apk
