f2db733511879a82b4e52ddf02cffb34127df3e7
dbottillo <dbottillo@gmail.com>
2016-02-15 15:49:02 +0000

    debug option from app

--- mtgSearch/src/main/java/com/dbottillo/mtgsearchfree/about/AboutFragment.java
+++ mtgSearch/src/main/java/com/dbottillo/mtgsearchfree/about/AboutFragment.java
@@ -7,20 +7,28 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dbottillo.mtgsearchfree.R;
 import com.dbottillo.mtgsearchfree.base.DBFragment;
 import com.dbottillo.mtgsearchfree.helper.LOG;
 import com.dbottillo.mtgsearchfree.helper.TrackingHelper;
+import com.dbottillo.mtgsearchfree.persistence.GeneralPreferences;
+
+import java.util.Calendar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 import static android.net.Uri.parse;
 
-public class AboutFragment extends DBFragment implements View.OnClickListener {
+public class AboutFragment extends DBFragment implements View.OnClickListener, View.OnTouchListener {
 
     String[] librariesName = new String[]{"Smooth Progress Bar", "Picasso", "LeakMemory"};
     String[] librariesAuthor = new String[]{"Castorflex", "Square", "Square"};
@@ -28,25 +36,40 @@ public class AboutFragment extends DBFragment implements View.OnClickListener {
 
     String versionName;
 
+    @Bind(R.id.share_app)
+    View shareApp;
+
+    @Bind(R.id.send_feedback)
+    Button sendFeedback;
+
+    @Bind(R.id.about_version)
+    TextView version;
+
+    @Bind(R.id.copyright)
+    TextView copyright;
+
+    @Bind(R.id.libraries_container)
+    LinearLayout cardContainer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_about, container, false);
+
+        ButterKnife.bind(this, v);
 
         setActionBarTitle(getString(R.string.action_about));
 
         versionName = "";
         try {
             versionName = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
-            TextView version = (TextView) v.findViewById(R.id.about_version);
             version.setText(Html.fromHtml("<b>" + getString(R.string.version) + "</b>: " + versionName));
         } catch (PackageManager.NameNotFoundException e) {
             LOG.d("[AboutFragment] exception: " + e.getLocalizedMessage());
         }
 
-        Button sendFeedback = (Button) v.findViewById(R.id.send_feedback);
         sendFeedback.setOnClickListener(this);
 
-        v.findViewById(R.id.share_app).setOnClickListener(new View.OnClickListener() {
+        shareApp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TrackingHelper.getInstance(getActivity()).trackEvent(TrackingHelper.UA_CATEGORY_UI, TrackingHelper.UA_ACTION_SHARE, "app");
@@ -59,7 +82,6 @@ public class AboutFragment extends DBFragment implements View.OnClickListener {
             }
         });
 
-        LinearLayout cardContainer = (LinearLayout) v.findViewById(R.id.libraries_container);
         for (int i = 0; i < librariesName.length; i++) {
             View libraryView = View.inflate(getContext(), R.layout.row_library, null);
             TextView title = (TextView) libraryView.findViewById(R.id.library_name);
@@ -81,7 +103,10 @@ public class AboutFragment extends DBFragment implements View.OnClickListener {
             });
         }
 
-        TextView copyright = (TextView) v.findViewById(R.id.copyright);
+        if (!GeneralPreferences.with(getActivity().getApplicationContext()).isDebugEnabled()) {
+            version.setOnTouchListener(this);
+        }
+
         copyright.setText(getString(R.string.copyright));
         return v;
     }
@@ -100,5 +125,31 @@ public class AboutFragment extends DBFragment implements View.OnClickListener {
     @Override
     public String getPageTrack() {
         return "/about";
+    }
+
+    private long firstTap;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent ev) {
+        switch (ev.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                firstTap = Calendar.getInstance().getTimeInMillis();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                long diff = Calendar.getInstance().getTimeInMillis() - firstTap;
+                long seconds = diff / 1000;
+                if (seconds < 5) {
+                    version.setOnTouchListener(null);
+                    GeneralPreferences.with(getActivity().getApplicationContext()).setDebug();
+                    Toast.makeText(getActivity(), R.string.debug_mode_active, Toast.LENGTH_LONG).show();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
---
+++ mtgSearch/src/main/java/com/dbottillo/mtgsearchfree/persistence/GeneralPreferences.java
@@ -0,0 +1,38 @@
+package com.dbottillo.mtgsearchfree.persistence;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class GeneralPreferences {
+
+    public static final String PREFS_NAME = "General";
+
+    private static final String DEBUG = "debug";
+
+    SharedPreferences sharedPreferences;
+
+    private static GeneralPreferences instance;
+
+    private GeneralPreferences() {
+
+    }
+
+    public static GeneralPreferences with(Context context) {
+        if (instance == null) {
+            instance = new GeneralPreferences(context);
+        }
+        return instance;
+    }
+
+    private GeneralPreferences(Context context) {
+        sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void setDebug() {
+        sharedPreferences.edit().putBoolean(DEBUG, true).apply();
+    }
+
+    public boolean isDebugEnabled() {
+        return sharedPreferences.getBoolean(DEBUG, false);
+    }
+}
