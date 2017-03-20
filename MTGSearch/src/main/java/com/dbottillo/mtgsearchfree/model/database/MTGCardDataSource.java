973fe73d831afccf5c978871a0ba5a5671ccc449
Daniele Bottillo <dbottillo@gmail.com>
2017-03-20 16:00:29 +0000

    libraries updated, version 2.8.2 with modern master 2017

--- MTGSearch/src/main/java/com/dbottillo/mtgsearchfree/model/database/MTGCardDataSource.java
+++ MTGSearch/src/main/java/com/dbottillo/mtgsearchfree/model/database/MTGCardDataSource.java
@@ -18,13 +18,13 @@ public class MTGCardDataSource {
     private static final int LIMIT = 400;
 
     enum STANDARD {
-        BATTLE_ZENDIKAR(15, "Battle for Zendikar"),
-        OATH_GATEWATCH(12, "Oath of the Gatewatch"),
-        SHADOWS_OVER_INNISTRAD(10, "Shadows over Innistrad"),
-        ELDRITCH_MOON(8, "Eldritch Moon"),
-        KALADESH(5, "Kaladesh"),
-        KALADESH_INVENTIONS(4, "Kaladesh Inventions"),
-        AETHER_REVOLT(1, "Aether Revolt");
+        BATTLE_ZENDIKAR(16, "Battle for Zendikar"),
+        OATH_GATEWATCH(13, "Oath of the Gatewatch"),
+        SHADOWS_OVER_INNISTRAD(11, "Shadows over Innistrad"),
+        ELDRITCH_MOON(9, "Eldritch Moon"),
+        KALADESH(6, "Kaladesh"),
+        KALADESH_INVENTIONS(5, "Kaladesh Inventions"),
+        AETHER_REVOLT(2, "Aether Revolt");
 
         public int setId;
         public String name;
