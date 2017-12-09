46a8816a28c5b150947e0fea444ccbe74ca7e3c9
dbottillo <dbottillo@gmail.com>
2017-12-09 20:53:38 +0000

    Close #226 Close #212 : add load image tracking and fix picasso and widget issue

---
+++ MTGSearch/src/main/java/com/dbottillo/mtgsearchfree/ui/views/MTGCardImageView.kt
@@ -0,0 +1,21 @@
+package com.dbottillo.mtgsearchfree.ui.views
+
+import android.content.Context
+import android.support.v7.widget.AppCompatImageView
+import android.util.AttributeSet
+
+class MTGCardImageView : AppCompatImageView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val height = (measuredWidth * MTGCardView.RATIO_CARD).toInt()
+        setMeasuredDimension(measuredWidth, height)
+    }
+
+}
