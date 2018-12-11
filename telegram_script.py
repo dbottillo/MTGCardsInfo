89374daeb9ef3ef008a3651ebe126a8709e109ca
dbottillo <dbottillo@gmail.com>
2018-12-11 20:57:33 +0000

    telegram script update

--- telegram_script.py
+++ telegram_script.py
@@ -1,7 +1,9 @@
-import telegram  #needs to run with python3
+import telegram  # needs to run with python3
+
+# it requires to run `pip install python-telegram-bot`
 
 bot = telegram.Bot(token='455430714:AAECvqeqRQ4evUVCBlsJdp3PciqjPSXSCyc')
 # print(bot.get_me())
 chatId = '-225997243'  # production
 # chatId = '-177269196'
-bot.send_message(chat_id=chatId, text="A new update is available v3.3.0: \n-Master 25")
+bot.send_message(chat_id=chatId, text="A new update is available v3.6.0: \n-Ultimate Masters")
