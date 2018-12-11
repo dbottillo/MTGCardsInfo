94400495d736d0717fb5b0d108a9597ce529d789
dbottillo <dbottillo@gmail.com>
2018-12-11 21:15:31 +0000

    remove python key

--- telegram_script.py
+++ telegram_script.py
@@ -1,8 +1,10 @@
 import telegram  # needs to run with python3
 
+from telegram_script_key import *
+
 # it requires to run `pip install python-telegram-bot`
 
-bot = telegram.Bot(token='455430714:AAECvqeqRQ4evUVCBlsJdp3PciqjPSXSCyc')
+bot = telegram.Bot(token=key)
 # print(bot.get_me())
 chatId = '-225997243'  # production
 # chatId = '-177269196'
