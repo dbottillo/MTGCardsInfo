2b535cdd4432154dc779c6fa7e44eb1e55b77c32
dbottillo <dbottillo@gmail.com>
2019-06-08 14:30:50 +0200

    fix modern horizons missing multiverse id

--- telegram_script.py
+++ telegram_script.py
@@ -8,4 +8,4 @@ bot = telegram.Bot(token=key)
 # print(bot.get_me())
 chatId = '-225997243'  # production
 # chatId = '-177269196'
-bot.send_message(chat_id=chatId, text="A new update is available v3.9.1: \n-War of the Spark")
+bot.send_message(chat_id=chatId, text="A new update is available v3.10.0: \n-Modern Horizons")
