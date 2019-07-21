69e207303b1c259d23586130d162b042e1444ff4
dbottillo <dbottillo@gmail.com>
2019-07-21 21:43:36 +0100

    app bundle configuration

--- release.sh
+++ release.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-./gradlew clean assembleRelease checkstyle pmd lintRelease detekt testReleaseUnitTest --no-daemon
-cp -r app/build/outputs/apk/release/app-release.apk ~/Google\ Drive/MTGCardsInfo/MTGSearch.apk
-cp -r app/build/outputs/apk/release/app-release.apk MTGSearch/release/MTGSearch-release.apk
-
+./gradlew clean bundleRelease checkstyle pmd lintRelease detekt testReleaseUnitTest --no-daemon
+cp -r app/build/outputs/bundle/release/app.aab ~/Google\ Drive/MTGCardsInfo/MTGSearch.aab
