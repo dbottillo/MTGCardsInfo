3599838cf68836fb19416ffb0e7493f413df5e39
dbottillo <dbottillo@gmail.com>
2022-01-31 10:52:30 +0000

    Fix crash widget api level 31

--- app/src/main/kotlin/com/dbottillo/mtgsearchfree/widget/LuckyWidgetProvider.kt
+++ app/src/main/kotlin/com/dbottillo/mtgsearchfree/widget/LuckyWidgetProvider.kt
@@ -10,7 +10,6 @@ import android.widget.RemoteViews
 import com.bumptech.glide.request.target.AppWidgetTarget
 import com.dbottillo.mtgsearchfree.Constants.RATIO_CARD
 import com.dbottillo.mtgsearchfree.GlideApp
-import com.dbottillo.mtgsearchfree.MTGApp
 import com.dbottillo.mtgsearchfree.R
 import com.dbottillo.mtgsearchfree.database.CardDataSource
 import com.dbottillo.mtgsearchfree.database.MTGCardDataSource
@@ -18,7 +17,6 @@ import com.dbottillo.mtgsearchfree.database.MTGDatabaseHelper
 import com.dbottillo.mtgsearchfree.lucky.CARD
 import com.dbottillo.mtgsearchfree.lucky.CardLuckyActivity
 import com.dbottillo.mtgsearchfree.model.MTGCard
-import com.dbottillo.mtgsearchfree.util.TrackingManagerImpl
 import com.google.gson.Gson
 
 class LuckyWidgetProvider : AppWidgetProvider() {
@@ -29,16 +27,22 @@ class LuckyWidgetProvider : AppWidgetProvider() {
         if (intent?.action.equals(REFRESH_ACTION)) {
             val manager = AppWidgetManager.getInstance(context)
             val cn = ComponentName(context, LuckyWidgetProvider::class.java)
-            val appWidgetIds = intent?.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
-                AppWidgetManager.INVALID_APPWIDGET_ID)?.let { intArrayOf(it) }
-                    ?: manager.getAppWidgetIds(cn)
+            val appWidgetIds = intent?.getIntExtra(
+                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID
+            )?.let { intArrayOf(it) }
+                ?: manager.getAppWidgetIds(cn)
             onUpdate(context, manager, appWidgetIds)
         }
 
         super.onReceive(context, intent)
     }
 
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
         val database = MTGDatabaseHelper(context).readableDatabase
         val cardDataSource = MTGCardDataSource(database, CardDataSource(database, Gson()))
 
@@ -56,23 +60,33 @@ class LuckyWidgetProvider : AppWidgetProvider() {
         val remoteViews = RemoteViews(context.packageName, R.layout.lucky_widget_layout)
         val width = context.resources.getDimensionPixelSize(R.dimen.widget_min_width)
         GlideApp
-                .with(context.applicationContext)
-                .asBitmap()
-                .load(card.scryfallImage)
-                .override(width, (width * RATIO_CARD).toInt())
-                .into(AppWidgetTarget(context, R.id.image_card, remoteViews, appWidgetId))
+            .with(context.applicationContext)
+            .asBitmap()
+            .load(card.scryfallImage)
+            .override(width, (width * RATIO_CARD).toInt())
+            .into(AppWidgetTarget(context, R.id.image_card, remoteViews, appWidgetId))
 
         // refresh
         val refreshIntent = Intent(context, LuckyWidgetProvider::class.java)
         refreshIntent.action = REFRESH_ACTION
         refreshIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-        val refreshPendingIntent = PendingIntent.getBroadcast(context, appWidgetId, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val refreshPendingIntent = PendingIntent.getBroadcast(
+            context,
+            appWidgetId,
+            refreshIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
         remoteViews.setOnClickPendingIntent(R.id.lucky_widget_more, refreshPendingIntent)
 
         // tap on image
         val openIntent = Intent(context, CardLuckyActivity::class.java)
         openIntent.putExtra(CARD, card.id)
-        val pendingIntent = PendingIntent.getActivity(context, appWidgetId, openIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            appWidgetId,
+            openIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
         remoteViews.setOnClickPendingIntent(R.id.image_card, pendingIntent)
 
         return remoteViews
