8b190fada4ed473b64f3a7af8e1f936acb2beb8e
Daniele <dbottillo@gmail.com>
2022-05-16 22:06:12 +0100

    Dependencies update, detekt config for all modules

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./gradlew clean spotlessCheck detekt bundleRelease app:lintRelease testReleaseUnitTest --no-daemon
+./gradlew clean detektAll bundleRelease app:lintRelease testReleaseUnitTest --no-daemon
 cp -r app/build/outputs/bundle/release/app-release.aab ~/Google\ Drive/My\ Drive/MTGCardsInfo/MTGSearch.aab
